2e8ea4fdc78413070b40b2de3fa27839efb2999b
gulugulubing <413153391@qq.com>
2025-07-21 18:00:58 -0600

    Gaofei's first commit

---
+++ LMS/Models/LMSModels/Enrollment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LMS.Models.LMSModels;
+
+public partial class Enrollment
+{
+    public string StudentUid { get; set; } = null!;
+
+    public string SubjectAbbr { get; set; } = null!;
+
+    public int CourseNumber { get; set; }
+
+    public uint Year { get; set; }
+
+    public string Season { get; set; } = null!;
+
+    public string? Grade { get; set; }
+
+    public virtual Class Class { get; set; } = null!;
+
+    public virtual Student StudentU { get; set; } = null!;
+}
